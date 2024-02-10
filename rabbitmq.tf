6cbff811ca08a9e533a9634fa6380d600bc734ef
Adnan <adnanshaikh11501@gmail.com>
2024-02-10 20:29:30 +0530

    PUSH

--- rabbitmq.tf
+++ rabbitmq.tf
@@ -12,7 +12,7 @@
 #   }
 # }
 
-# Creates EC2 Instance
+# Creates OD EC2 Instance
 resource "aws_instance" "rabbitmq" {
   ami                     = data.aws_ami.image.id
   instance_type           = var.RABBITMQ_NODE_TYPE
@@ -40,38 +40,3 @@ resource "null_resource" "app_install" {
      ]
   }  
 }
-
-# # Provisions Redis/Elastic-Cache Cluster
-
-# resource "aws_elasticache_cluster" "example" {
-#   cluster_id              = "roboshop-${var.ENV}-redis"
-#   engine                  = var.REDIS_ENGINE
-#   node_type               = var.REDIS_NODE_TYPE
-#   num_cache_nodes         = var.REDIS_NODE_COUNT
-#   parameter_group_name    = aws_elasticache_parameter_group.redis.name
-#   engine_version          = var.REDIS_ENGINE_VERSION
-#   port                    = var.REDIS_PORT
-#   subnet_group_name       = aws_elasticache_subnet_group.redis.name
-#   security_group_ids      = [aws_security_group.allows_redis.id]
-# }
-
-# # Creates a subnet-groups
-# resource "aws_elasticache_subnet_group" "redis" {
-#   name       = "roboshop-${var.ENV}-redis-subnet-group"
-#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
-#   tags = {
-#     Name = "roboshop-${var.ENV}-redis-subnet-group"
-#   }
-# }
-
-# # Provisions Redis/Elastic-Cache Parameter Group
-# resource "aws_elasticache_parameter_group" "redis" {
-#   name   = "roboshop-${var.ENV}-redis-pg"
-#   family = var.REDIS_ENGINE_FAMILY
-# }
-
-
-
-  
-
