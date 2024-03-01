cafa1d2b49d41b80d3f1cedd9c64ad44bfa549ca
Adnan <adnanshaikh11501@gmail.com>
2024-03-01 18:07:08 +0530

    ami changed

--- data.tf
+++ data.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "vpc" {
 
 data "aws_ami" "image" {
   most_recent = true
-  name_regex  = "centos7-with-ansible"
+  name_regex  = "DevOps-LabImage-Centos-8"
   owners      = ["355449129696"] 
 }
 
